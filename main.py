ce58735409f089a2a8386ef3332cbb01a073c371
Jeff555max <vadik98765@gmail.com>
2025-01-31 18:05:45 +0500

    1

---
+++ main.py
@@ -0,0 +1,6 @@
+import asyncio  # Для работы с асинхронными функциями
+from aiogram import Bot, Dispatcher # Bot отвечает за взаимодействие с Telegram bot API,
+                                    # Dispatcher управляет обработкой входящих сообщений и команд.
+
+
+
